f416c677e21d5f3b1e0d534aad9afbe510cc44b6
markwh1te <iamwh1temark@outlook.com>
2017-05-15 01:30:15 -0700

    half way

--- nnet/nnet.py
+++ nnet/nnet.py
@@ -1,7 +1,4 @@
-
-In [1]:
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
-%matplotlib inlinekkj
--- nnet/nnet_NGclass.py
+++ nnet/nnet_NGclass.py
@@ -21,8 +21,12 @@ print(y[0],y_onehot[0,:])
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
+# Sigmoid gradient
+def sigmoidGradient(x):
+    return (sigmoid(x)*(1-sigmoid))
+
 # feedforward
-def feedforward(X,theta1,theta2):
+def forward_propagate(X,theta1,theta2):
     m,n = X.shape
 
     a1 = np.insert(X,0,values=np.ones(m),axis=1)
@@ -34,3 +38,69 @@ def feedforward(X,theta1,theta2):
     return a1,z2,a2,z3,h
 
 # cost function
+def cost(params,input_size,hidden_size,num_labels,X,y,learning_rate):
+    m = X.shape[0]
+    x = np.matrix(X)
+    y = np.matrix(y)
+
+    # unroll params
+    theta1 = np.matrix(np.reshape(params[:hidden_size * (input_size + 1)], (hidden_size, (input_size + 1))))
+   
+
+    # run feed forward pass
+    a1, z2, a2, z3, h = forward_propagate(X, theta1, theta2)
+
+
+    # compute cost with without for loop
+    J = 0
+    for i in range(m):
+        first_term = np.multiply(-y[i,:], np.log(h[i,:]))
+        second_term = np.multiply((1 - y[i,:]), np.log(1 - h[i,:]))
+        J += np.sum(first_term - second_term)
+
+    J = J/m
+
+    J += (float(learning_rate) / (2 * m)) * (np.sum(np.power(theta1[:,1:], 2)) + np.sum(np.power(theta2[:,1:], 2)))
+    return J
+
+input_size = 400
+hidden_size = 25
+num_labels = 10
+learning_rate = 1
+
+params = (np.random.random(size=hidden_size * (input_size + 1) + num_labels * (hidden_size + 1)) - 0.5) * 0.25
+
+
+m = X.shape[0]
+X = np.matrix(X)
+y = np.matrix(y)
+
+
+
+theta1 = np.matrix(np.reshape(params[:hidden_size * (input_size + 1)], (hidden_size, (input_size + 1))))
+theta2 = np.matrix(np.reshape(params[hidden_size * (input_size + 1):], (num_labels, (hidden_size + 1))))
+
+a1, z2, a2, z3, h = forward_propagate(X, theta1, theta2)
+
+# def backprop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
